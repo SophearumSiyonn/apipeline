a559b5cd5d1a1336c4d9ebcc83fe37ecdf012441
Sophearum Siyonn <2023420siyonn@aupp.edu.kh>
2026-03-08 17:03:13 +0700

    added api

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const e1 = require('express');
 const app = e1();
 
 //SEARCH API
+app.get('/search', (req, res) => {
+    res.send('<html><body>INSIDE SEARCH API..</body></html>');
+    });
+
+//VIEW API
 app.get('/view', (req, res) => {
-    res.send('<html><body>helllooo worllldddd</body></html>');
-});
+    res.send('<html><body>INSIDE VIEW API..</body></html>');
+    });
+    
+//DELETE API
+app.delete('/del', (req, res) => {
+    res.send('<html><body>INSIDE DELETE API..</body></html>');
+    });
 
 
 // START THE EXPRESS SERVER. 5000 is the PORT NUMBER
