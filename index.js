84bfe4107c5b83f6e70cc27b02f968c060637ef9
Sophearum Siyonn <2023420siyonn@aupp.edu.kh>
2026-03-08 15:52:55 +0700

    Reformat index.js for consistency

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ app.get('/view', (req, res) => {
 }); 
 
 
+
 // START THE EXPRESS SERVER. 5000 is the PORT NUMBER
 app.listen(5001, () =>
     console.log('EXPRESS Server Started at Port No: 5001'));
+
 
 
 
